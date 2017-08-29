6a6823e20f87015ea85216e9534a493d3de14d58
Matte <mattias4@kth.se>
2017-08-30 00:39:04 +0200

    Minor touchups

--- caesar.py
+++ caesar.py
@@ -4,36 +4,41 @@ Caesar "crypto".
  - Assumes a dictionary in '/usr/share/dict/words'.
  - Converts everything to UPPER CASE.
 
-    usage: python3 caesar.py [key]
-        - If key is given, encrypt STDIN with the given key.
-        - If no key is given, attempt to guess which key was used to encrypt STDIN.
+ usage: python3 caesar.py [key]
+    - If key is given, encrypt STDIN with the given key.
+    - If no key is given, attempt to guess which key was used to encrypt STDIN.
 """
 import sys
 from collections import namedtuple
 from string import ascii_uppercase
 
-Guess = namedtuple('guess', 'key, score')
-
 ALPHABET = list(ascii_uppercase)
 
-
-def encrypt(msg, key):
-    def shift_char(c):
-        if c in ALPHABET:
-            return ALPHABET[(ALPHABET.index(c) + key) % len(ALPHABET)]
-        else:
-            return c
-
-    return ''.join(shift_char(c) for c in list(msg))
+Guess = namedtuple('guess', 'key, score')
 
 
-def decrypt(msg, key):
-    return encrypt(msg, -key)
+def encrypt(plaintext, key=1):
+    """Encrypts the given plaintext message using a Caesar Cipher."""
+
+    def shift_char(char):
+        try:
+            return ALPHABET[(ALPHABET.index(char) + key) % len(ALPHABET)]
+        except ValueError:
+            return char
+
+    return ''.join(shift_char(char) for char in list(plaintext))
 
 
-def score(cipher_text, key, words):
-    recognized_words = sum(1 for word in decrypt(cipher_text, key).split() if word in words)
-    return recognized_words / len(cipher_text.split())
+def decrypt(ciphertext, key):
+    """Decrypts the Caesar Ciphertext using the given key."""
+    return encrypt(ciphertext, -key)
+
+
+def score(ciphertext, key, dictionary):
+    """Scores the given key using dictionary lookup of the decrypted words."""
+    words = decrypt(ciphertext, key).split()
+    real_words = [word for word in words if word in dictionary]
+    return len(real_words) / len(words)
 
 
 if __name__ == '__main__':
@@ -41,14 +46,15 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         key = int(sys.argv[1])
 
-        cipher = encrypt(indata, key)
-        print(cipher)
+        ciphertext = encrypt(indata, key)
+        print(ciphertext)
     else:
         with open('/usr/share/dict/words') as f:
-            words = [word.upper().strip() for word in f.readlines()]
+            dictionary = [word.upper().strip() for word in f.readlines()]
+
         guess = Guess(key=0, score=0)
         for key in range(len(ALPHABET)):
-            new_score = score(indata, key, words)
+            new_score = score(indata, key, dictionary)
             print(f'Score for {key}: \t{new_score:.2f}')
             if new_score > guess.score:
                 guess = Guess(key=key, score=new_score)
