971aa069880e361104752f013890343e4f380b98
Matte <mattias4@kth.se>
2017-08-30 00:15:22 +0200

    Fun with Caesar!

---
+++ README.md
@@ -0,0 +1,48 @@
+# Fun with Caesar Cipher
+
+```
+usage: python3 caesar.py [key]
+	- If key is given, encrypt STDIN with the given key.
+	- If no key is given, attempt to guess which key was used to encrypt STDIN.
+```
+
+## Encrypt
+
+```
+$ echo "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'S BACK 1234567890" | python3 caesar.py 7
+AOL XBPJR IYVDU MVE QBTWLK VCLY AOL SHGF KVN'Z IHJR 1234567890
+```
+
+## Decypt
+
+```
+$ echo "AOL XBPJR IYVDU MVE QBTWLK VCLY AOL SHGF KVN'Z IHJR 1234567890" | python3 caesar.py
+Score for 0: 	0.00
+Score for 1: 	0.00
+Score for 2: 	0.00
+Score for 3: 	0.00
+Score for 4: 	0.09
+Score for 5: 	0.00
+Score for 6: 	0.00
+Score for 7: 	0.73
+Score for 8: 	0.00
+Score for 9: 	0.00
+Score for 10: 	0.00
+Score for 11: 	0.09
+Score for 12: 	0.00
+Score for 13: 	0.00
+Score for 14: 	0.18
+Score for 15: 	0.00
+Score for 16: 	0.00
+Score for 17: 	0.00
+Score for 18: 	0.00
+Score for 19: 	0.00
+Score for 20: 	0.18
+Score for 21: 	0.09
+Score for 22: 	0.00
+Score for 23: 	0.00
+Score for 24: 	0.00
+Score for 25: 	0.00
+My guess is that the key was: 7!
+THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'S BACK 1234567890
+```
---
+++ caesar.py
@@ -0,0 +1,57 @@
+"""
+Caesar "crypto".
+
+ - Assumes a dictionary in '/usr/share/dict/words'.
+ - Converts everything to UPPER CASE.
+
+    usage: python3 caesar.py [key]
+        - If key is given, encrypt STDIN with the given key.
+        - If no key is given, attempt to guess which key was used to encrypt STDIN.
+"""
+import sys
+from collections import namedtuple
+from string import ascii_uppercase
+
+Guess = namedtuple('guess', 'key, score')
+
+ALPHABET = list(ascii_uppercase)
+
+
+def encrypt(msg, key):
+    def shift_char(c):
+        if c in ALPHABET:
+            return ALPHABET[(ALPHABET.index(c) + key) % len(ALPHABET)]
+        else:
+            return c
+
+    return ''.join(shift_char(c) for c in list(msg))
+
+
+def decrypt(msg, key):
+    return encrypt(msg, -key)
+
+
+def score(cipher_text, key, words):
+    recognized_words = sum(1 for word in decrypt(cipher_text, key).split() if word in words)
+    return recognized_words / len(cipher_text.split())
+
+
+if __name__ == '__main__':
+    indata = sys.stdin.read().upper()
+    if len(sys.argv) > 1:
+        key = int(sys.argv[1])
+
+        cipher = encrypt(indata, key)
+        print(cipher)
+    else:
+        with open('/usr/share/dict/words') as f:
+            words = [word.upper().strip() for word in f.readlines()]
+        guess = Guess(0, 0)
+        for key in range(len(ALPHABET)):
+            new_score = score(indata, key, words)
+            print(f'Score for {key}: \t{new_score:.2f}')
+            if new_score > guess.score:
+                guess = Guess(key, new_score)
+
+        print(f'My guess is that the key was: {guess.key}!')
+        print(decrypt(indata, guess.key))
