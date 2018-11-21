a8c91b7fa75527f272c448b777c47daf29a0b5e1
Matte <mattias4@kth.se>
2018-11-21 21:27:42 +0100

    Updated unhash.py, and added a benchmark that just calculates the hashes

---
+++ benchmark.py
@@ -0,0 +1,4 @@
+import hashlib
+
+for _ in range(4049500):
+    hashlib.sha256('somesalt000000-0000'.encode())
--- unhash.py
+++ unhash.py
@@ -1,14 +1,14 @@
 import hashlib
-import sys
 from itertools import product
 
-SALT = 'somesalt'.encode()
-start = int(sys.argv[1])
-end = int(sys.argv[2])
-digest = sys.argv[3]
+from pandas import date_range
 
-for year, month, day, seq in product(range(start, end), range(13), range(32), range(10000)):
-    pnr = f'{year:02d}{month:02d}{day:02d}-{seq:04d}'
-    if digest == hashlib.sha256(SALT + pnr.encode()).hexdigest():
-        print(f'SHA256({digest}) = {pnr}')
-        break
+SALT = 'somesalt'.encode()
+DIGEST = 'b93d35d303b71ce6b0d27f6b858454de1dd6956b911bbf83c59ab81bd0c70f38'
+
+dates = date_range('1900-01-01', '1999-12-31')
+for date, seq in product(dates, range(10000)):
+    pnr = date.strftime("%y%m%d-") + format(seq, "04d")
+    digest = hashlib.sha256(SALT + pnr.encode()).hexdigest()
+    if DIGEST == digest:
+        print('Found:', pnr)
